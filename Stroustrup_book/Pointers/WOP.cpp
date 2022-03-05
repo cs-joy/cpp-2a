23c6255d01a5ddc4346ecc4b9138dca0a6de14a6
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-03-05 08:21:52 +0600

    Pointers Introduction
    Working of C++ Pointers

---
+++ Stroustrup_book/Pointers/WOP.cpp
@@ -0,0 +1,29 @@
+// Working of C++ Pointers
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int var = 5;
+
+    //declare pointer variable
+    int* pointVar;
+
+    //store address of var
+    pointVar = &var;
+
+    //print value of var
+    cout << "var = " << var << endl;
+
+    //print address of var
+    cout << "Address of var (&var) = " << &var << endl << endl;
+
+    //print pointer of pointVar
+    cout << "pointVar = " << pointVar << endl;
+
+    //print the content of the address pointVar points to
+    cout << "Content of the address pointed to by pointVar (*pointVar) = " << *pointVar <<endl;
+
+    return 0;
+
+}
