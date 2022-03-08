0495ca396c3ea0e4a9be037c55863ab8762b1fab
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-03-08 14:59:00 +0600

    initial commit

--- Stroustrup_book/Arrays/AoP.cpp
+++ Stroustrup_book/Arrays/AoP.cpp
@@ -1,32 +1,12 @@
-#include<iostream>
-#include<conio>
-void main()
-{
-   clrscr();
-   int *iptr[5];
-   int fa=65, fb=66, fc=67, fd=68, fe=69;
-   int i;
+#include <iostream>
+using namespace std;
 
-   // initialize array pointers by making them point to 5 different ints
-   iptr[0] = &fa;
-   iptr[1] = &fb;
-   iptr[2] = &fc;
-   iptr[3] = &fd;
-   iptr[4] = &fe;
+int main() {
+   int srr[5] = {9, 6, 2, 8, 4};
 
-   // now prints the values being pointed to by the pointers
-   for(i=0; i<5; i++)
-   {
-      cout<<"The pointer iptr["<<i<<"] points to "<<*iptr[i]<<"\n";
-   }
-   cout<<"\n";
-
-   // now print the addresses stored in the array
-   cout<<"The base address of the array iptr of pointers is "<<iptr<<"\n";
-   for(i=0; i<5; i++)
-   {
-      cout<<"The address stored in iptr["<<i<<"] is "<<iptr[i]<<"\n";
+   for (int i = 0; i < 5; i++ /* i++ = ++i */) {
+      cout << "srr[" << i << "] - " << srr[i] << " " << endl;
    }
 
-   getch();
+   return 0;
 }
