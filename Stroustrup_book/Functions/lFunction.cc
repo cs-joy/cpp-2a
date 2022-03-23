c841b7b7f25a24c8fa087a8f9869236c98dcdd37
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-03-24 05:02:41 +0600

    definition of cc library function

---
+++ Stroustrup_book/Functions/lFunction.cc
@@ -0,0 +1,15 @@
+//lFunction- C++ Library Function
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double number, squareRoot;
+    number = 25.0;
+    // sqrt() is a library function to calculate the square root
+    squareRoot = sqrt(number);
+    cout << "Sqaure root of " << number << " is: " << squareRoot << endl;
+
+    return 0;
+}
