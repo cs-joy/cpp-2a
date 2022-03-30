612172cc3e1147dc7382b370ef3470f0e5d22061
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-03-30 19:37:35 +0600

    des added into that co.

--- cplusplus/arrays/arraysASparameters.cpp
+++ cplusplus/arrays/arraysASparameters.cpp
@@ -19,3 +19,12 @@ int main() {
 
     return 0;
 }
+
+/*
+
+int the code above, the first parameter (int arg[]) accepts any array whose elements are of type int, whatever its length. 
+For that reason, we have included a second parameter that tells the function the length of each array that we pass to it as its first parameter. 
+This allows the for loop that prints out the array to know the range to iterate in the array passed, without going out of range.
+
+
+*/
