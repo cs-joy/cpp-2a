1a539121a8941f25d41c6f587e6e4791146f4ef0
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-01 01:55:09 +0600

    Create initialize.cc

---
+++ cplusplus/arrays/characterSequence/initialize.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()  {
+    char myword[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+
+    cout << "myword[]: " << myword << endl;
+
+    return 0;
+}
