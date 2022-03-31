f96814acb826b81b8ad917ab9d94bface9298b0c
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-01 02:06:43 +0600

    string literals add

--- cplusplus/arrays/characterSequence/initialize.cc
+++ cplusplus/arrays/characterSequence/initialize.cc
@@ -4,8 +4,11 @@ using namespace std;
 
 int main()  {
     char myword[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+    //string literals
+    char sj[] = "Hello";
 
-    cout << "myword[]: " << myword << endl;
+    cout << "myword[]: " << myword << " " << sizeof(myword) << endl;
+    cout << "sj[]: " << sj << " " << sizeof(sj) << endl;
 
     return 0;
 }
