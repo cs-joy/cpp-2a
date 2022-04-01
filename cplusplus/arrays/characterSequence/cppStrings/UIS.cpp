43f7e7b229d0a27969712c3d16a8347d49bd51ce
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-01 15:45:00 +0600

    Create UIS.cpp

---
+++ cplusplus/arrays/characterSequence/cppStrings/UIS.cpp
@@ -0,0 +1,14 @@
+//User Input Strings
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string firstName;
+    cout << "Write your first name: ";
+    cin >> firstName;
+    cout << "Your name is: " << firstName << endl;
+
+    return 0;
+}
