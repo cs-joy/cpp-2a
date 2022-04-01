02cf7bdfc22f15a21f7f253c26ed2d1a033c33d2
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-01 15:55:51 +0600

    Create ON.cpp

---
+++ cplusplus/arrays/characterSequence/cppStrings/ON.cpp
@@ -0,0 +1,10 @@
+//Omitting Namespace
+#include <iostream>
+#include <string>
+
+int main() {
+    std :: string greeting = "Hello";
+    std :: cout << "greeting= " << greeting ;
+
+    return 0;
+}
