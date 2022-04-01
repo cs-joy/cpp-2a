6d426fc4e22015d15f01d4e1cc8bbbaa51967045
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-01 10:46:08 +0600

    Create stringLength.cpp
    * using v.length()
    * using v.size()
    - where v is a string variable

---
+++ cplusplus/arrays/characterSequence/cppStrings/stringLength.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    //using length()
+    cout << "The length of the text string is: " << text.length() << endl;
+    //using size()
+    cout << "The length of the text string is: " << text.size() << endl;
+
+
+    return 0;
+}
