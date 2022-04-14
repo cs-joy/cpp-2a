dc37e6291f2e880af6a1a4f7329980212356d165
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-15 02:50:48 +0600

    Create breakContinue.cpp

---
+++ Note/breakContinue.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    //while
+    int num;
+    cout << "Please type a non-negative integer: ";
+    cin >> num;
+    /*
+    while(num <=10) {
+        cout << num << " \n";
+        num--;
+
+        if(num == 5) {
+            break;
+        }
+
+    }
+    */
+
+    //do...while
+    /*do {
+        num--;
+        if(num < 7) {
+            break;
+        }
+        cout << "num= " << num << endl;
+    } while(num <= 15);
+
+    cout << "false~\n";
+
+    */
+
+   //for...if
+
+   for(int i =0; i <= 1000; i++) {
+       cout << num << "\n";
+       num--;
+       if(num < 5) {
+           break;
+       }
+   }
+
+
+    return 0;
+}
