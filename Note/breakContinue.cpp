41c89f70a78866dcf108707858eead5b273c3d4e
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-04-15 16:24:52 +0600

    Update breakContinue.cpp

--- Note/breakContinue.cpp
+++ Note/breakContinue.cpp
@@ -60,5 +60,46 @@ int main() {
    }
    cout << "Sum = " << sum <<endl;
 
+
+   //continue statement
+   cout << "\n--continue statement--\n";
+   int a;
+   cout << "A= ";
+   cin >> a;
+   while(a < 10) {
+       
+       a++;
+       if(a == 7) {
+           continue;
+       }
+       cout << "aA= " << a << endl;
+   }
+
+   //do...while
+   cout << "\ndo....while\n";
+   int j;
+   cout << "j= ";
+   cin >> j;
+   
+   do {
+       j++;
+       if(j%2 == 0) {
+           continue;
+       }
+       cout << "odd j= " << j << endl;
+   } while(j < 100);
+
+   //for
+   cout << "\nfor\n";
+   int k;
+   cout << "k= ";
+   cin >> k;
+   for(int i=0; i < k; i++) {
+       if(i%2 != 0) {
+           continue;
+       }
+       cout << "even i=" << i << endl;
+   }
+
     return 0;
 }
