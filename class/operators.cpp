529fb21206e19f25ad807869dc024e0c71ba3ee0
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-06-25 16:57:09 +0600

    Update operators.cpp

--- class/operators.cpp
+++ class/operators.cpp
@@ -3,20 +3,35 @@ using namespace std;
 
 int main(){
 
-    int num = 3, num2 = 6;
-    int sub, div;
+    double num, num2, sub, div;
+    int loop;
+    cout << "loop ";
+    cin >> loop;
 
-    if (!(num > num2)){
-        sub = num2 - num;
-        cout << "subtraction of " << num2 << " and " << num << " is= " << sub << endl;
-        div = num2 / num;
-        cout << "division of " << num2 << " and " << num << " is= " << div;
-    } else {
-        sub = num - num2;
-        cout << "subtraction of " << num << " and " << num2 << " is= " << sub << endl;
-        div = num / num2;
-        cout << "division of " << num << " and " << num2 << " is= " << div;
+    while(--loop){
+        cout << "input first number: ";
+        cin >> num;
+
+        cout << "input second number:";
+        cin >> num2;
+
+        if (!(num > num2))
+        {
+            sub = num2 - num;
+            cout << "subtraction of " << num2 << " and " << num << " is= " << sub << endl;
+            div = num2 / num;
+            cout << "division of " << num2 << " and " << num << " is= " << div << endl;
+        }
+        else
+        {
+            sub = num - num2;
+            cout << "subtraction of " << num << " and " << num2 << " is= " << sub << endl;
+            div = num / num2;
+            cout << "division of " << num << " and " << num2 << " is= " << div << endl;
+        }
     }
+
+    
 
     return 0;
 }
