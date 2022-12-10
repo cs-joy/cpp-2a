50aaf6feb74a1bd865f2abde451a90a9f3abc934
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2022-12-10 07:58:05 +0600

    Update hashTable_Function.cpp

--- hashTable_Function.cpp
+++ hashTable_Function.cpp
@@ -7,6 +7,8 @@ int main()
 {
     // find out index of an array where the array size is 11
     // index number = (sum_of_ASCII_codes) Mod (array_size)
+    // our array is:: { Bea, Tim, Leo, Sam, Mia, Zoe, Jan, Lou, Max, Ada, Ted }
+    
     int x1, x2, x3; // for storing ASCII values
     x1 = 77, x2 = 105, x3 = 97; // // ASCII value
     int a = x1 + x2 + x3;
